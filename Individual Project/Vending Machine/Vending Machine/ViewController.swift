9e6ff9e1f902eaf4019bf4e4934a0dcf1082b9d0
waitnsee <yellow.cjh@gmail.com>
2017-09-17 21:37:17 +0900

    자판기 과제 1차버전 완료

--- Individual Project/Vending Machine/Vending Machine/ViewController.swift
+++ Individual Project/Vending Machine/Vending Machine/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 0.   UI 타이틀 및 텍스트
+    
     @IBOutlet weak var appTitle: UILabel!
     @IBOutlet weak var basketMoney: UILabel!
     @IBOutlet weak var currentBalance: UILabel!
@@ -17,6 +19,8 @@ class ViewController: UIViewController {
     
     
     override func viewDidLoad() {
+        
+    // 1.   초기 화면 뿌려질 부분
         super.viewDidLoad()
         currentBalance.text = "0"
         appTitle.text = "천원 마켓"
@@ -27,11 +31,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // 2.   변수 선언
     var balance: Int = 0
     var charged: Int = 1000
     var estimatedBuying: Int = 0
     var pricePerItem: Int = 1000
     
+    
+    // 3.   물건 선택
+    // 3-1. 터치시마다 장바구니 금액 증가 및 최초 터치시 메세지 변경
     @IBAction func items (btn: UIButton)
     {
         estimatedBuying += pricePerItem
@@ -39,6 +48,13 @@ class ViewController: UIViewController {
         greetingMsg.text = "다 먹고 싶죠? (방긋)"
     }
     
+    // 4.   물건 구매 (조건 충족 필요)
+    // 4-1. 잔액이 장바구니 금액보다 크거나 같고 (and) 장바구니 금액이 0이 아닐때
+    //      잔액에서 장바구니 금액만큼 차감
+    //      장바구니 금액은 장바구니 금액만큼 차감
+    //      장바구니 금액은 초기화해서 출력
+    // 4-2. 잔액이 장바구니 금액보다 작을때 "돈 가져오세요" 출력
+    // 4-3. 장바구니 금액이 0일때는 "아직 안고르셨음" 출력
     @IBAction func purchase (btn: UIButton) {
         if  balance >= estimatedBuying && estimatedBuying != 0 {
             balance -= estimatedBuying
@@ -46,7 +62,6 @@ class ViewController: UIViewController {
             self.currentBalance.text = String(balance)
             self.greetingMsg.text = "또 올거죠? (애원)"
             estimatedBuying = 0
-            self.basketMoney.text = String(estimatedBuying)
             
         } else if balance < estimatedBuying && estimatedBuying != 0 {
             self.greetingMsg.text = "돈 가져오세요 (빠직)"}
@@ -56,11 +71,13 @@ class ViewController: UIViewController {
         
     }
     
+    // 5. 잔액 충전 : 터치시마다 천원씩 증가
     @IBAction func charge (btn: UIButton) {
         balance += charged
         self.currentBalance.text = String(balance)
     }
     
+    // 6. 구매 초기화 : 터치시 장바구니 금액 및 환영메세지 초기화
     @IBAction func reset (btn: UIButton) {
         estimatedBuying = 0
         self.basketMoney.text = String(estimatedBuying)
