236e4ea042b08833a7e0b38e074e28f7d9603c8f
waitnsee <yellow.cjh@gmail.com>
2017-09-17 20:45:23 +0900

    자판기 과제 완료

--- class test/class test/Calculator.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Calculator.swift
-//  class test
-//
-//  Created by Choi Jeong Hoon on 2017. 9. 13..
-//  Copyright © 2017년 JH Factory. All rights reserved.
-//
-
-import Foundation
--- subject.swift
+++
@@ -1,9 +0,0 @@
-//
-//  subject.swift
-//  
-//
-//  Created by Choi Jeong Hoon on 2017. 9. 12..
-//
-//
-
-import Foundation
