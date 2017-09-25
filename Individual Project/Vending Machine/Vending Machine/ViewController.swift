7fc2347f2f9fb73de72c4bd90bc84737b3b31623
waitnsee <yellow.cjh@gmail.com>
2017-09-26 00:23:48 +0900

    isKnowing 업데이트

--- Individual Project/Vending Machine/Vending Machine/ViewController.swift
+++ Individual Project/Vending Machine/Vending Machine/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var greetingMsg: UILabel!
     
     override func viewDidLoad() {
-        
+        U
         // 1.   초기 화면 뿌려질 부분
         super.viewDidLoad()
         currentBalance.text = "0"
@@ -44,7 +44,6 @@ class ViewController: UIViewController {
     enum MenuOfVendingMachine: Int {
         case candybar=1, chips, cookie, dietsoda, fruitjuice, gum, poptart, sandwich, water
     }
-    let someMenu = MenuOfVendingMachine.candybar.rawValue
     
     // 3.   물건 선택
     // 3-1. 터치시마다 장바구니 금액 증가 및 최초 터치시 메세지 변경
@@ -52,53 +51,51 @@ class ViewController: UIViewController {
     {
         
         let itemType = MenuOfVendingMachine(rawValue: btn.tag)
-        
         if let itemNewtype = itemType{
-        switch itemNewtype {
-    
-        case .candybar :
-            pricePerItem = 500
-        case .chips :
-            pricePerItem = 1500
-        case .cookie :
-            pricePerItem = 1000
-        case .dietsoda :
-            pricePerItem = 2000
-        case .fruitjuice :
-            pricePerItem = 1500
-        case .gum :
-            pricePerItem = 500
-        case .poptart :
-            pricePerItem = 1000
-        case .sandwich:
-            pricePerItem = 500
-        case .water :
-            pricePerItem = 1500
-            
-        default:
-            break}
+            switch itemNewtype {
+                
+            case .candybar :
+                pricePerItem = 500
+            case .chips :
+                pricePerItem = 1500
+            case .cookie :
+                pricePerItem = 1000
+            case .dietsoda :
+                pricePerItem = 2000
+            case .fruitjuice :
+                pricePerItem = 1500
+            case .gum :
+                pricePerItem = 500
+            case .poptart :
+                pricePerItem = 1000
+            case .sandwich:
+                pricePerItem = 500
+            case .water :
+                pricePerItem = 1500
+                
+            default:                 // default가 왜 절대 실행되지 않는다고 나오는지
+                break
+            }
         }
         guard let optionalInt = pricePerItem else { return }
         estimatedBuying += optionalInt
         basketMoney.text = String(estimatedBuying)
-
-     }
-        // 4.   물건 구매 (조건 충족 필요)
-        // 4-1. 잔액이 장바구니 금액보다 크거나 같고 (and) 장바구니 금액이 0이 아닐때
-        //      잔액에서 장바구니 금액만큼 차감
-        //      장바구니 금액은 장바구니 금액만큼 차감
-        //      장바구니 금액은 초기화해서 출력
-        // 4-2. 잔액이 장바구니 금액보다 작을때 "돈 가져오세요" 출력
-        // 4-3. 장바구니 금액이 0일때는 "아직 안고르셨음" 출력
         
-        @IBAction func purchase (btn: UIButton) {
-            if var balanced = balance {
+    }
+    // 4.   물건 구매 (조건 충족 필요)
+    // 4-1. 잔액이 장바구니 금액보다 크거나 같고 (and) 장바구니 금액이 0이 아닐때
+    //      잔액에서 장바구니 금액만큼 차감
+    //      장바구니 금액은 장바구니 금액만큼 차감
+    //      장바구니 금액은 초기화해서 출력
+    // 4-2. 잔액이 장바구니 금액보다 작을때 "돈 가져오세요" 출력
+    // 4-3. 장바구니 금액이 0일때는 "아직 안고르셨음" 출력
+    
+    @IBAction func purchase (btn: UIButton) {
+        if var balanced = balance {
             if  balanced >= estimatedBuying && estimatedBuying != 0 {
-                print(balanced)
-                print(estimatedBuying)
                 balanced -= estimatedBuying
                 balance = balanced      //
-                self.basketMoney.text = "0"
+                self.basketMoney.text = "0"     // self를 넣었을때와 안넣었을때 의미상의 차이는? <이하 동일>
                 self.currentBalance.text = String(balanced)
                 self.greetingMsg.text = "또 올거죠? (애원)"
                 estimatedBuying = 0 }
@@ -106,13 +103,13 @@ class ViewController: UIViewController {
                 self.greetingMsg.text = "돈 가져오세요 (빠직)"}
             else if estimatedBuying == 0 {
                 self.greetingMsg.text = "아직 안고르셨음 (민망)"}
-            }
         }
+    }
     
-        // 5. 잔액 충전 : 터치시마다 천원/이천원씩 증가
-        @IBAction func charge (btn: UIButton) {
-            
-            if var balanced = balance {
+    // 5. 잔액 충전 : 터치시마다 천원/이천원씩 증가
+    @IBAction func charge (btn: UIButton) {
+        
+        if var balanced = balance {
             switch btn.tag {
             case 1 :
                 balanced += charged
@@ -123,23 +120,26 @@ class ViewController: UIViewController {
                 balance = balanced
                 self.currentBalance.text = String(balanced)
             default : currentBalance.text = "0"}
-           }else {
-                // 과제 : 숫자를 직접 입력하지 않고 balance를 넣되 describe쓰지 않고 오류 없이 해결하기
-                switch btn.tag {
-                case 1 :
-                    balance = charged
-                    self.currentBalance.text = String(1000)
-                    
-                case 2 :
-                    balance = charged * 2
-                    self.currentBalance.text = String(2000)
-                default :
-                       return}
-            }
+        }else {
+            // describing이 뭔지?  String()로 변환이 안될때 쓰는거 같은데 안될떄가 어떤경우들이 있는지
+            switch btn.tag {
+            case 1 :
+                balance = charged
+                self.currentBalance.text = String(describing:balance)
+                
+            case 2 :
+                balance = charged * 2
+                self.currentBalance.text = String(describing:balance)
+            default :
+                return}
         }
-        // 6. 구매 초기화 : 터치시 장바구니 금액 및 환영메세지 초기화
-        @IBAction func reset (btn: UIButton) {
-            estimatedBuying = 0
-            self.basketMoney.text = String(estimatedBuying)
-            self.greetingMsg.text = "다 먹고 싶죠? (의미심장)"}
     }
+    // 6. 구매 초기화 : 터치시 장바구니 금액 및 환영메세지 초기화
+    @IBAction func reset (btn: UIButton) {
+        self.estimatedBuying = 0
+        self.basketMoney.text = String(estimatedBuying)
+        self.greetingMsg.text = "다 먹고 싶죠? (의미심장)"}
+}
+
+
+UIResponder
--- Study/isKnowingTest/isKnowingTest/ViewController.swift
+++ Study/isKnowingTest/isKnowingTest/ViewController.swift
@@ -12,38 +12,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var result1: UILabel!
     @IBOutlet weak var result2: UILabel!
     @IBOutlet weak var result3: UILabel!
-    
-    // 변수 선언은 반드시 이곳에 해줘야 하는지 알았음
-    
-    // 함수 위치를 여기에 써도 되는지 viewDidLoad안에 써도 되는지? 다른 파일에 넣어도 되는지 헷갈렸음
-    // 함수를 다른 스위프트 파일에 넣어도 오류가 안남
-    // 헷갈렸던 점 화면에 결과값 표시해주기
-    // 최초시도 : 함수 호출해서 인풋값 넣지 않고, result text에 바로 resultValue대입 = 초기값 0 출력
-    // 두번째 시도 : resultValue아래에 함수호출 = 초기값 0 출력
-    // 해결 : result.text위에서 함수호출
-    
-    override func viewDidLoad() {
-        
-        var answerSheet: Breakthrough = Breakthrough()
-        var firstResult = answerSheet.reducingNum(inputNum: 11223344)
-        var secondResult = answerSheet.chekLeapYear(inputYear: 2100)
-        var thirdResult = answerSheet.addDigit(inputNum: 123455)
-      
-        
-    }
-    
-    // 처음에 버튼 눌렀을때 텍스트 뿌려주는 방법을 생각하기 힘들었음
-    
-    @IBAction func firstAnswer(_ sender: UIButton) {
-        result1.text = String()
-    }
-    
+    @IBOutlet weak var result4: UILabel!
 
+    let answerSheet: Breakthrough = Breakthrough()
+    
+    override func viewDidLoad() {  // 뷰가 보여지고 뷰에 액션을 하기전에 보여질 가장 최적의 장소
+        super.viewDidLoad()
+    }
+    @IBAction func firstAnswer(_ sender: UIButton) {
+        result1.text = String(answerSheet.reducingNum(inputNum: 11223344))
+    }
+    @IBAction func secondAnswer(_ sender: UIButton) {
+        result2.text = String(answerSheet.chekLeapYear(inputYear: 2115))
+    }
+    @IBAction func thirdAnswer(_ sender: UIButton) {
+        result3.text = String(answerSheet.addDigit(inputNum: 123456))
+    }
+    @IBAction func fourthAnswer(_ sender: UIButton) {
+        result4.text = String(describing: answerSheet.randomNum())
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 }
-
-
-
